638f882c461ce1fecb5427a099380158d7fac65f
phothinmg <phothinmg@gmail.com>
2025-05-04 23:29:08 +0630

    modified:   readme.md

--- readme.md
+++ readme.md
@@ -1,9 +1,14 @@
-# Lwe8-text
+<div align="center">
+<img src="assets/textile-wordmark.svg">
+<h1>lwe8-textile</h1>
+</div>
+
+![npm: license](https://flat.badgen.net/npm/license/lwe8-textile)
 
 ## Content
 
-[Overview](#overviews)
+[Overview](#overview)
 
-## Overviews
+## Overview
 
 
--- src/index.ts
+++ src/index.ts
@@ -6,60 +6,63 @@ import type {
   TextileVisitor,
   TextileExtensionFn,
   Root,
+  TextileExtension,
 } from "lwe8-textile-types";
 
 // types
+
 function treeWalk(tree: Root, callback: TextileVisitor) {
   visit(tree, callback);
   return tree;
 }
-class Textile {
-  private _extensions: TextileExtensionFn[];
+export default class Textile {
+  private _extensions: TextileExtension[];
   private _tree: Root | {};
   private _html: string;
   private _text: string;
-  private _visitor: TextileVisitor[];
+  private _visitors: TextileVisitor[];
 
-  constructor(input: string) {
+  constructor() {
     this._extensions = [];
     this._tree = {};
     this._html = "";
-    this._text = input;
-    this._visitor = [];
+    this._text = "";
+    this._visitors = [];
   }
   private _int() {
+    if (this._text === "") {
+      throw new Error("You must input text to convert");
+    }
     const _html = textile.parse(this._text);
     this._tree = fromHtml(_html, { fragment: true });
     if (this._extensions.length > 0) {
       for (const ext of this._extensions) {
-        this._tree = treeWalk(this._tree as Root, ext().walk);
+        this._tree = treeWalk(this._tree as Root, ext.walk);
       }
     }
-    if (this._visitor.length > 0) {
-      for (const visitor of this._visitor) {
+    if (this._visitors.length > 0) {
+      for (const visitor of this._visitors) {
         this._tree = treeWalk(this._tree as Root, visitor);
       }
     }
     this._html = toHtml(this._tree as Root);
   }
-  use(ext: TextileExtensionFn) {
-    this._extensions.push(ext);
+
+  use(ext: TextileExtensionFn | TextileExtension) {
+    const _ext = typeof ext === "function" ? ext() : ext;
+    this._extensions.push(_ext);
     return this;
   }
   visit(visitor: TextileVisitor) {
-    this._visitor.push(visitor);
+    this._visitors.push(visitor);
     return this;
   }
-  get html() {
+  parse(text: string) {
+    this._text = text;
     this._int();
-    return this._html;
+    return {
+      html: this._html,
+      hast: this._tree,
+    };
   }
-  get hast() {
-    this._int();
-    return this._tree as Root;
-  }
-}
-
-export function lwe8Textile(text: string) {
-  return new Textile(text);
 }
